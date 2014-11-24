0e0a3b8c26ac9aa15bb2c365a693b62b7d488cf9
Luis Fernando Barrera <luisfernando@informind.com>
2014-11-24 15:03:18 -0600

    Random Int

---
+++ src/com/informind/RandomInt.java
@@ -0,0 +1,13 @@
+package com.informind;
+
+public class RandomInt {
+    public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]);
+
+        double r = Math.random();
+
+        int n = (int) (r * N);
+
+        System.out.println(n);
+    }
+}
