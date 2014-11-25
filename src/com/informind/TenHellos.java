e7679d8e2fad4cf3d6e5c0e863e6bcfe0c3b88cb
Luis Fernando Barrera <luisfernando@informind.com>
2014-11-24 19:02:56 -0600

    Ten Hellos

---
+++ src/com/informind/TenHellos.java
@@ -0,0 +1,18 @@
+package com.informind;
+
+/**
+ * Created by luisfernando on 24/11/14.
+ */
+public class TenHellos {
+    public static void main(String[] args) {
+        System.out.println("First Hello");
+        System.out.println("Second Hello");
+        System.out.println("Third Hello");
+        int i = 4;
+
+        while (i <= 10) {
+            System.out.println(i + "th Hello");
+            i = i +1;
+        }
+    }
+}
